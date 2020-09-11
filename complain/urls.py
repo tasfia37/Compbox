8c9e2ecaf4a85eac4ac4c9fdc0ad31bcbca20dfa
hasnainkarim <47083396+hasnainkarim@users.noreply.github.com>
2020-09-11 20:27:47 +0600

    urls.py updated

--- complain/urls.py
+++ complain/urls.py
@@ -2,15 +2,24 @@ from . import views
 from django.urls import path
 
 
+
 urlpatterns=[
             path('create',views.create,name='create'),
             path('<int:complain_id>', views.detail, name='detail'),
-            path('<int:complain_id>/deletecomplain', views.deletecomplain, name='deletecomplain'),
             path('m<int:complain_id>', views.mdetail, name='mdetail'),
-            path('m<int:complain_id>/approvecomplain', views.approvecomplain, name='approvecomplain'),
             path('clist', views.clist, name='clist'),
-            path('approvedlist', views.approvedlist, name='approvedlist'),
-            # path('mclist', views.mclist, name='mclist'),
+            path('lastweek', views.lastweek, name='lastweek'),
+            path('last15days', views.lastweek, name='last15days'),
+            path('lastmonth', views.lastweek, name='lastmonth'),
             path('mhome', views.mhome, name='mhome'),
+            path('ministrypage', views.getmins2, name='ministrypage'),
+           # path('create', views.getmins, name='create'),
 
+            path('<int:complain_id>/deletecomplain', views.deletecomplain, name='deletecomplain'),
+            # path('mclist', views.mclist, name='mclist'),
+            # path('mhome', views.getadd, name='add'),
+
+            #tasfiar last add
+            path('approvedlist', views.approvedlist, name='approvedlist'),
+            path('m<int:complain_id>/approvecomplain', views.approvecomplain, name='approvecomplain'),
 ]
